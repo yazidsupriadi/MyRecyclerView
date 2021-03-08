b68018a54f9c2a785b0e561480cee8c3ac42e23e
yazidsupriadi <yazidsupriadi14@gmail.com>
2021-03-08 13:24:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.kt
@@ -0,0 +1,41 @@
+package com.example.myrecyclerview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+
+class GridHeroAdapter(val listHero: ArrayList<Hero>): RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
+    override fun onCreateViewHolder(viewGroup: ViewGroup, p1: Int): GridViewHolder {
+        val view: View = LayoutInflater.from(viewGroup.context).inflate(R.layout.item_grid_hero, viewGroup, false)
+        return GridViewHolder(view)
+    }
+    override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
+        Glide.with(holder.itemView.context)
+            .load(listHero[position].photo)
+            .apply(RequestOptions().override(350, 550))
+            .into(holder.imgPhoto)
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
+
+    }
+
+    override fun getItemCount(): Int {
+        return listHero.size
+    }
+    inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+
+    private lateinit var onItemClickCallback: OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClicked(data: Hero)
+    }
+}
